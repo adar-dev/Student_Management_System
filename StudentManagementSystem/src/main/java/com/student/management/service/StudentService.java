1072660e61ba2cc947334d1d111d4b6a24e361c1
lab-user <lab-user@ip-172-31-15-115.ap-south-1.compute.internal>
2022-10-19 11:03:35 +0530

    model created

--- StudentManagementSystem/src/main/java/com/student/management/service/StudentService.java
+++ StudentManagementSystem/src/main/java/com/student/management/service/StudentService.java
@@ -1,5 +1,20 @@
 package com.student.management.service;
 
-public class StudentService {
+ 
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.student.management.model.Student;
+import com.student.management.repository.StudentRepository;
+ 
+@Service
+@Transactional
+public class StudentService {
+ 
+   
 }
