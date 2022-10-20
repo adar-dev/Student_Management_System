d25d9804b47b9f1f5cfc98c0b943d827402f5670
sharma7mansi <sharma.0.mansi.7@gmail.com>
2022-10-20 14:47:31 +0530

    "frontend code without integration with backend"

--- react_projects/student-management-system/src/App.js
+++ react_projects/student-management-system/src/App.js
@@ -4,12 +4,25 @@ import './App.css';
 import Navbar from "./layout/Navbar";
 import axios from "axios";
 import Home from "./pages/Home";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import AddStudent from "./students/AddStudent";
+import UpdateStudent from "./students/UpdateStudent";
 
 function App() {
   return (
     <div className="App">
+    <Router>
     <Navbar/>
-    <Home/>
+
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/addStudent" element={<AddStudent/>}/>
+      <Route exact path="/updateStudent/:id" element={<UpdateStudent/>}/>
+
+    </Routes>
+  
+    </Router>
+    
     </div>
   );
 }
--- react_projects/student-management-system/src/layout/Navbar.js
+++ react_projects/student-management-system/src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar()
 {
@@ -6,13 +7,13 @@ export default function Navbar()
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
              <div className="container-fluid">
-              <a className="navbar-brand" href="#">
+              <Link className="navbar-brand" to="/">
                 Student Management System
-              </a>
+              </Link>
              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
              <span className="navbar-toggler-icon"></span>
             </button> 
-            <button className="btn btn-outline-light">Add Student</button>
+            <Link className="btn btn-outline-light" to="addStudent">Add Student</Link>
            </div>
           </nav>
         </div>
